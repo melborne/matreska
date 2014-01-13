183943afcbf4505ba7b182ed6fda8c5a55098d85
kyoendo <postagie@gmail.com>
2014-01-13 22:25:25 +0900

    Add basic tests

--- lib/matryoshka.rb
+++ lib/matryoshka.rb
@@ -1,57 +1,62 @@
-class Matryoshka
-  attr_reader :figures
-  def initialize(core)
-    @figures = []
-    @core = core
-  end
+require "matryoshka/version"
 
-  def set(*figures)
-    figures.each { |fig| @figures << fig }
-    self
-  end
-
-  def call(env=nil)
-    build.call(env)
-  end
-  alias :[] :call
-
-  def build
-    @figures.inject(@core) { |core, fig| fig.new(core) }
-  end
-  private :build
-
-  def clear
-    @figures.clear
-  end
-
-  class << self
-    def figure(name, *meth, &blk)
-      if blk
-        _figure(name, &blk)
-      else
-        _figure(name) do |res, env|
-          res.send(*meth).tap { |r| p r }
-        end
-      end
+module Matryoshka
+  class Builder
+    attr_reader :dolls
+    def initialize(core)
+      @dolls = []
+      @core = core
     end
-    alias :doll :figure
 
-    private
-    def _figure(name, &blk)
-      klass = Class.new do
-        def initialize(core)
-          @core = core
+    def set(doll, *args, &blk)
+      @dolls << ->core{ doll.new(core, *args, &blk) }
+      self
+    end
+
+    def call(env=nil)
+      build.call(env)
+    end
+    alias :[] :call
+
+    def build
+      @dolls.inject(@core) { |core, doll| doll.call(core) }
+    end
+    private :build
+
+    def clear
+      @figures.clear
+    end
+
+    class << self
+      def figure(name, *meth, &blk)
+        if blk
+          _figure(name, &blk)
+        else
+          _figure(name) do |res, env|
+            res.send(*meth).tap { |r| p r }
+          end
         end
-  
-        define_method(:call) do |env|
-          res = @core[env]
-          blk[res, env]
-        end
-        alias :[] :call
       end
-      Object.const_set(name.to_s, klass)
+      alias :doll :figure
+
+      private
+      def _figure(name, &blk)
+        klass = Class.new do
+          def initialize(core)
+            @core = core
+          end
+  
+          define_method(:call) do |env|
+            res = @core[env]
+            blk[res, env]
+          end
+          alias :[] :call
+        end
+        Object.const_set(name.to_s, klass)
+      end
     end
   end
 end
 
 M8a = Matryoshka
+
--- spec/matryoshka_spec.rb
+++ spec/matryoshka_spec.rb
@@ -1,11 +1,71 @@
 require 'spec_helper'
 
+class PlusOne
+  def initialize(core)
+    @core = core
+  end
+
+  def call(env)
+    res = @core.call(env)
+    res + 1
+  end
+end
+
+class Plus
+  def initialize(core, arg, &blk)
+    @core = core
+    @arg = arg
+  end
+
+  def call(env)
+    res = @core.call(env)
+    res + @arg
+  end
+end
+
 describe Matryoshka do
   it 'should have a version number' do
     Matryoshka::VERSION.should_not be_nil
   end
 
-  it 'should do something useful' do
-    false.should be_true
+  describe Matryoshka::Builder do
+    before do
+      @core = ->env{ env }
+      @ma = Matryoshka::Builder.new(@core)
+    end
+    describe "#new" do
+      it 'takes a core object which has call method' do
+        expect(@ma.instance_variable_get "@core").to eq @core
+      end
+    end
+
+    describe "#call" do
+      it 'execute call method of core object' do
+        expect(@ma.call(1)).to eq 1
+      end
+
+      it 'execute call of core and dolls' do
+        @ma.set PlusOne
+        expect(@ma.call(1)).to eq 2
+      end
+
+      it 'execute call of core and dolls with its arguments' do
+        @ma.set Plus, 2
+        expect(@ma.call(1)).to eq 3
+      end
+    end
+
+    describe "#set" do
+      it 'sets doll objects which takes a core or doll and has call method' do
+        @ma.set PlusOne
+        dolls = @ma.dolls
+        expect(dolls.size).to eq 1
+        expect(dolls.first).to respond_to(:call)
+      end
+
+      it 'set doll objects with its args and a block' do
+        expect{ @ma.set Plus, 2 }.not_to raise_error
+      end
+    end
   end
 end
